a26fd7bb05b50030215b1250af395cedc3b1513d
bbeck89 <60022459+bbeck89@users.noreply.github.com>
2020-06-30 23:54:43 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/us74dd7nqdy2ir4/Hot_Dog_Model.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/us74dd7nqdy2ir4/Hot_Dog_Model.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['Hotdog', 'Not hotdog']
